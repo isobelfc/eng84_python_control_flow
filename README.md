2ba9664c7f8e61059f834263ed8421e5db4278f7
Isobel Fitt-Conway <isobelfc@gmail.com>
2021-03-24 11:27:57 +0000

    for loops

--- README.md
+++ README.md
@@ -7,5 +7,51 @@
 - Multiple `elif`s may be present, and each one is checked in order until one is met and executes
 - If none of the conditions are met, the `else` block is executed
 
-## For loops
-## While loops
+## Loops
+
+- Loops allow an action to be repeated multiple times - they iterate through the data
+- They can be used with all kinds of data collections
+- We use them to reduce the amount of duplicate code written, which saves time and reduces mistakes
+- In addition, the number of repeats may not be a set number, e.g. dependent on the length of a list, or a certain condition being met
+- Loops allow the number of repeats to be flexible
+- `break` is a key word that will end the loop
+
+### For loops
+- We used a `for` loop to iterate through a list, and compared it to the process of manually repeating the line
+```python
+shopping_list = ["bread", "eggs", "milk", "oranges"]
+
+print(shopping_list)
+print(shopping_list[0])
+print(shopping_list[1])
+print(shopping_list[2])
+print(shopping_list[3])
+
+# Let's use a for loop to iterate through our list
+for items in shopping_list:
+    print(items)
+```
+- We used a `for` loop to iterate through letters
+```python
+for letter in "fruits":
+    print(letter)
+```
+- We used the key word `break` to end the loop when the condition was true
+```python
+shopping_list = ["bread", "eggs", "milk", "oranges"]
+for items in shopping_list:
+    print(items)
+    if items == "milk":
+        break
+        # at this point when milk is found when iterating the loop will stop
+```
+- We used a `for` loop with a dictionary to print name and bill amount for each person
+```python
+food_bill = {1: {"name": "James", "bill": "£1"}, 2: {"name": "Bond", "bill": "£2"}, 3: {"name": "Shah", "bill": "£3"}}
+
+# Print the names and the bill amount for each person
+for items in food_bill.values():
+    print(items["name"] + ": " + items["bill"])
+```
+
+### While loops
--- control_flow.py
+++ control_flow.py
@@ -18,24 +18,58 @@
 # casting implemented - age as integer
 # display the age back to the user with appropriate message
 
-age = int(input("Please enter your age "))
-print(f"You are {age} years old")
+# age = int(input("Please enter your age "))
+# print(f"You are {age} years old")
+#
+# ratings = {
+#     "Finding Nemo": 0,
+#     "Jurassic Park": 0,
+#     "Return of the King": 12,
+#     "Blade Runner": 15,
+#     "Trainspotting": 18
+# }
+#
+# film = input("Which film do you want to see? Currently playing: Finding Nemo, Jurassic Park, Return of the King, Blade Runner, Trainspotting ")
+#
+# minimum_age = ratings[film]
+#
+# if age >= minimum_age:
+#     print("Thank you, please proceed")
+# elif age < minimum_age:
+#     print("Sorry, you are too young to watch this film")
+# else:
+#     print("Sorry, something went wrong, please try again")
 
-ratings = {
-    "Finding Nemo": 0,
-    "Jurassic Park": 0,
-    "Return of the King": 12,
-    "Blade Runner": 15,
-    "Trainspotting": 18
-}
+# Loops
+# Syntax to create a loop
+# for is python keyword variable then data_collection
 
-film = input("Which film do you want to see? Currently playing: Finding Nemo, Jurassic Park, Return of the King, Blade Runner, Trainspotting ")
+# shopping_list = ["bread", "eggs", "milk", "oranges"]
+#
+# print(shopping_list)
+# print(shopping_list[0])
+# print(shopping_list[1])
+# print(shopping_list[2])
+# print(shopping_list[3])
+#
+# # Let's use a for loop to iterate through our list
+# for items in shopping_list:
+#     print(items)
 
-minimum_age = ratings[film]
+# Let's iterate through letters in a word
+# for letter in "fruits":
+#     print(letter)
 
-if age >= minimum_age:
-    print("Thank you, please proceed")
-elif age < minimum_age:
-    print("Sorry, you are too young to watch this film")
-else:
-    print("Sorry, something went wrong, please try again")
+# shopping_list = ["bread", "eggs", "milk", "oranges"]
+# for items in shopping_list:
+#     print(items)
+#     if items == "milk":
+#         # break is a key word
+#         break  # at this point when milk is found when iterating the loop will stop
+
+# Let's create a dictionary of our food bill so we can iterate through it
+food_bill = {1: {"name": "James", "bill": "£1"}, 2: {"name": "Bond", "bill": "£2"}, 3: {"name": "Shah", "bill": "£3"}}
+
+# Print the names and the bill amount for each person
+for items in food_bill.values():
+    print(items["name"] + ": " + items["bill"])
